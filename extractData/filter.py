b709a8a0984ce8635a4e190df3f25fcb4a70e845
MathEnthusiast314 <89387162+MathEnthusiast314@users.noreply.github.com>
2022-01-03 06:57:09 +0530

    filtering files from reddit, discord
    reddit: https://gist.github.com/Kiran-Raj-Dev/9fe9056c86a5df8684c0e8ef1179bab1 by @ad_chaos 
    discord: https://github.com/Tyrrrz/DiscordChatExporter

---
+++ extractData/filter.py
@@ -0,0 +1,53 @@
+import json
+import re
+import os
+'https://discord.com/channels/@me/897118245692264478/926739865503154206'
+
+directory = 'Unoffcial Desmos Discord Server extracted files'
+data_good_s = []
+for filename in os.scandir(directory):
+    if filename.is_file():
+    
+        with open(filename.path, encoding='utf-8') as f:
+            data_s = json.load(f)
+
+        
+        pattern = re.compile(r"(https://www.desmos.com/calculator/.{10})")
+
+
+        for j in range(len(data_s['messages'])):
+            result_s = pattern.search(data_s['messages'][j]['content'])
+
+            if result_s is not None:
+                objj={"content":result_s.group(1),"author":data_s['messages'][j]['author']['name']}
+            
+            
+                data_good_s.append(objj)
+
+
+        with open("submissions_filtered.json","w", encoding='utf-8') as s:
+            json.dump(data_good_s, s, ensure_ascii=False, indent=4)       
+
+
+'''filter_with_only_one_file
+with open("adocument.json", encoding='utf-8') as f:
+    data_s = json.load(f)
+
+data_good_s = []
+pattern = re.compile(r"(https://www.desmos.com/calculator/.{10})")
+
+
+for j in range(len(data_s['messages'])):
+    result_s = pattern.search(data_s['messages'][j]['content'])
+
+    if result_s is not None:
+        objj={"content":result_s.group(1),"author":data_s['messages'][j]['author']['name']}
+        
+        
+        data_good_s.append(objj)
+
+
+with open("submissions_filtered.json","w", encoding='utf-8') as s:
+    json.dump(data_good_s, s, ensure_ascii=False, indent=4)
+
+'''
